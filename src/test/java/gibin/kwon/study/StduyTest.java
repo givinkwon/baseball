a548b913bf3015dbf45fdf86118095e3b977dca0
givinkwon <gibin.kwon@kurlycorp.com>
2022-05-04 13:41:47 +0900

    refactor : 테스트 명 변경

--- src/test/java/gibin/kwon/study/StduyTest.java
+++ src/test/java/gibin/kwon/study/StduyTest.java
@@ -15,35 +15,37 @@ import static org.springframework.test.util.AssertionErrors.assertTrue;
 
 public class StduyTest {
 
+
     @Test
-    @DisplayName("splitTest")
+    @DisplayName("스피릿 함수 사용 테스트2")
     void splitTest() {
         String source = "1,2";
-        List<String> goal = Arrays.asList("1","2");
+        List<String> goal = Arrays.asList("1", "2");
         List<String> result = Arrays.asList(source.split(","));
         assertThat(result.equals(goal));
     }
 
     @Test
-    @DisplayName("splitContainTest")
+    @DisplayName("스피릿 함수 사용 테스트2")
     void splitContainTest() {
         String source = "1";
         List<String> goal = Arrays.asList("1");
         List<String> result = Arrays.asList(source.split(","));
+//        assertThat(result).containsExactly(String.valueOf(goal));
         assertThat(result.equals(goal));
     }
 
     @Test
-    @DisplayName("subStringTest")
+    @DisplayName("부분 문자열 테스트")
     void subStringTest() {
         String source = "(1,2)";
         String goal = "1,2";
-        String result = source.substring(1,3);
+        String result = source.substring(1, 3);
         assertThat(result.equals(goal));
     }
 
     @Test
-    @DisplayName("charAtTest")
+    @DisplayName("해당 문자가 포함되었는 지 테스트")
     void charAtTest() {
         String source = "abc";
         Character goal_A = 'a';
@@ -64,18 +66,18 @@ public class StduyTest {
     }
 
     @Test
-    @DisplayName("setSizeTest")
+    @DisplayName("사이즈가 맞는 지 테스트")
     void setSizeTest() {
         Set<String> source = new HashSet<>(Arrays.asList("a", "b", "c"));
         assertThat(source.size()).isEqualTo(3);
     }
 
-    @DisplayName("setContainTest")
+    @DisplayName("포함된값이 있는 지 테스트")
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3})
-    //@CsvSource(value = {"1: true", "2: true", "3 : true", "4:false","5:false"}, delimiter = ':')
+        //@CsvSource(value = {"1: true", "2: true", "3 : true", "4:false","5:false"}, delimiter = ':')
     void setContainTest(int element) {
-        Set<Integer> source = new HashSet<>(Arrays.asList(1,2,3));
+        Set<Integer> source = new HashSet<>(Arrays.asList(1, 2, 3));
         assertThat(source.contains(element)).isTrue();
         assertThat(source.contains(4)).isFalse();
         assertThat(source.contains(5)).isFalse();
